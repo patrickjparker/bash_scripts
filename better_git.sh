538e6737263b0e9e5d2bf6d23a4a789e11ac6c6c
Patrick Parker <patrickjpark@gmail.com>
2025-04-18 23:09:01 -0600

    Add stash function

--- better_git.sh
+++ better_git.sh
@@ -1,10 +1,11 @@
-# Using fzf to make common git commands and workflows for interactive
+# Using fzf to make common git commands and workflows for interactive meant to be put in .bashrc and called with git alias
+# dependancies: git, fzf, less
 
 git_switch_interactive() {
   local branch
   branch=$(
-    git branch --format='%(refname:short)' | grep -v "^\*" |
-      fzf --height 40% --reverse \
+    git branch --format='%(refname:short)' --sort=-committerdate |
+      fzf --height 40% --reverse --no-sort \
         --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" --color=always {}' \
         --preview-window=right:50%:wrap \
         --bind 'ctrl-l:execute(git log --date=short --pretty="format:%C(auto)%cd %h%d %s" --color=always {} | less -r)' \
@@ -19,9 +20,43 @@ git_switch_interactive() {
 }
 
 # Create function for git stash
-# ctl-y yanks select stash name
-# ctl-l inspects the stash
-# clt-d drop (maybe a confirmation popup?)
+git_stash_interactive() {
+  local copy_cmd
+  case "$(uname -s)" in
+  Darwin*) copy_cmd="pbcopy" ;;
+  Linux*) copy_cmd="xclip -selection clipboard 2>/dev/null || xsel --clipboard 2>/dev/null" ;;
+  MINGW* | MSYS*) copy_cmd="clip.exe" ;;
+  *) copy_cmd="echo 'Clipboard not supported'" ;;
+  esac
+
+  local options=()
+  if [[ "$1" == "drop" ]]; then
+    options=(--multi)
+  elif [ -n "$1" ]; then
+    echo option not recognized: "$1"
+    return 1
+  fi
+  local stash=$(
+    git stash list |
+      fzf --height 40% --reverse \
+        "${options[@]}" \
+        --no-sort \
+        --preview 'git stash show --stat -p --color=always {1}' \
+        --preview-window=right:50%:wrap \
+        --bind 'ctrl-l:execute(git stash show --stat -p --color=always {1} | less -r)' \
+        --bind "ctrl-y:execute-silent(echo {1} | pbcopy)+change-header('Copied {1} to clipboard!')" \
+        --header 'ctrl-l to view full log, ctrl-y to copy' \
+        --delimiter ":" |
+      awk -F: '{print $1}'
+  )
+  if [ -n "$stash" ]; then
+    if [[ "$1" == "drop" ]]; then
+      echo "$stash" | xargs git stash drop
+    else
+      echo "$stash" | xargs git stash pop
+    fi
+  fi
+}
 
 # Create function for git add
 # ctl-l inspect (like normal)
